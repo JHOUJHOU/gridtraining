cac819bc8bbe5004f756c2eb82f004921389b497
JHOUJHOU <a01450887@gmail.com>
2021-08-20 22:34:09 +0800

    增加展覽頁面

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -2,19 +2,6 @@ $(function() {
   console.log('Hello Bootstrap5');
 });
 
-// 選單
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 9,
-  spaceBetween: 30,
-  // pagination: {
-  //   el: ".swiper-pagination",
-  //   clickable: true,
-  // },
-  768:{
-    slidesPerView: 3,
-    spaceBetween: 30,
-  },
-});
 
 //猜你也喜歡 
 var swiper = new Swiper(".mySwiper1", {
